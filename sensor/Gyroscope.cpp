f1d33ed3947dc1ca3eca7a7ad930297f97409aad
yangcheng <yangcheng@zuzuche.com>
2019-01-15 14:35:27 +0800

    陀螺仪计算DCM，与四元数有冲突，移到Quaternions里

--- sensor/Gyroscope.cpp
+++ sensor/Gyroscope.cpp
@@ -8,41 +8,41 @@
 
 using namespace Eigen;
 
-/**
- * 从陀螺仪获取姿态旋转矩阵(方向余弦矩阵DCM), b系坐标转g系
- *
- * @param gyro, 陀螺仪欧拉角w(x,y,z);
- * @param deltaT
- * @return 方向余弦矩阵DCM
- */
-Matrix3d Gyroscope::GetDCM(Vector3d &gyro, double &deltaT) {
-
-    // 计算deltaT时间内角度变化值，deltaT越小越精确。
-    double delta_wx = gyro(0) * deltaT;
-    double delta_wy = gyro(1) * deltaT;
-    double delta_wz = gyro(2) * deltaT;
-
-    double cos_delta_wx = cos(delta_wx);
-    double sin_delta_wx = sin(delta_wx);
-    double cos_delta_wy = cos(delta_wy);
-    double sin_delta_wy = sin(delta_wy);
-    double cos_delta_wz = cos(delta_wz);
-    double sin_delta_wz = sin(delta_wz);
-
-    // 计算DCM矩阵(z * y * x)。
-    Matrix3d DCM;
-    DCM(0, 0) = cos_delta_wy * cos_delta_wz;
-    DCM(0, 1) = -sin_delta_wz * cos_delta_wx + sin_delta_wx * sin_delta_wy * cos_delta_wz;
-    DCM(0, 2) = sin_delta_wx * sin_delta_wz + sin_delta_wy * cos_delta_wx * cos_delta_wz;
-    DCM(1, 0) = sin_delta_wz * cos_delta_wy;
-    DCM(1, 1) = cos_delta_wx * cos_delta_wz + sin_delta_wx * sin_delta_wy * sin_delta_wz;
-    DCM(1, 2) = -sin_delta_wx * cos_delta_wz + sin_delta_wy * sin_delta_wz * cos_delta_wx;
-    DCM(2, 0) = -sin_delta_wy;
-    DCM(2, 1) = sin_delta_wx * cos_delta_wy;
-    DCM(2, 2) = cos_delta_wx * cos_delta_wy;
-
-    return DCM;
-}
+///**
+// * 从陀螺仪获取姿态旋转矩阵(方向余弦矩阵DCM), b系坐标转g系
+// *
+// * @param gyro, 陀螺仪欧拉角w(x,y,z);
+// * @param deltaT
+// * @return 方向余弦矩阵DCM
+// */
+//Matrix3d Gyroscope::GetDCM(Vector3d &gyro, double &deltaT) {
+//
+//    // 计算deltaT时间内角度变化值，deltaT越小越精确。
+//    double delta_wx = gyro(0) * deltaT;
+//    double delta_wy = gyro(1) * deltaT;
+//    double delta_wz = gyro(2) * deltaT;
+//
+//    double cos_delta_wx = cos(delta_wx);
+//    double sin_delta_wx = sin(delta_wx);
+//    double cos_delta_wy = cos(delta_wy);
+//    double sin_delta_wy = sin(delta_wy);
+//    double cos_delta_wz = cos(delta_wz);
+//    double sin_delta_wz = sin(delta_wz);
+//
+//    // 计算DCM矩阵(z * y * x)。
+//    Matrix3d DCM;
+//    DCM(0, 0) = cos_delta_wy * cos_delta_wz;
+//    DCM(0, 1) = -sin_delta_wz * cos_delta_wx + sin_delta_wx * sin_delta_wy * cos_delta_wz;
+//    DCM(0, 2) = sin_delta_wx * sin_delta_wz + sin_delta_wy * cos_delta_wx * cos_delta_wz;
+//    DCM(1, 0) = sin_delta_wz * cos_delta_wy;
+//    DCM(1, 1) = cos_delta_wx * cos_delta_wz + sin_delta_wx * sin_delta_wy * sin_delta_wz;
+//    DCM(1, 2) = -sin_delta_wx * cos_delta_wz + sin_delta_wy * sin_delta_wz * cos_delta_wx;
+//    DCM(2, 0) = -sin_delta_wy;
+//    DCM(2, 1) = sin_delta_wx * cos_delta_wy;
+//    DCM(2, 2) = cos_delta_wx * cos_delta_wy;
+//
+//    return DCM;
+//}
 
 /**
  * 陀螺仪标定,测量零飘误差.
