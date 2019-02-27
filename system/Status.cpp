910b70637f42bfbbbc1194f901f61fd5e0806c0f
yangcheng <yangcheng@zuzuche.com>
2019-02-27 11:52:25 +0800

    初始化更新

--- system/Status.cpp
+++ system/Status.cpp
@@ -3,6 +3,13 @@
 //
 
 #include "Status.h"
+#include "../math/Quaternions.h"
+#include "../sensor/Gyroscope.h"
+#include "../sensor/Accelerometer.h"
+#include "../sensor/Magnetometer.h"
+#include "Eigen/Dense"
+
+using namespace Eigen;
 
 Position Status::GetPosition() const {
     return this->position;
@@ -18,4 +25,44 @@ Velocity Status::GetVelocity() const {
 
 Parameters Status::GetParameters() const {
     return this->parameters;
+}
+
+void Status::Init() {
+    this->velocity.v_x = 0.0;
+    this->velocity.v_y = 0.0;
+    this->velocity.v_z = 0.0;
+
+    this->position.x = 0.0;
+    this->position.y = 0.0;
+    this->position.z = 0.0;
+
+    this->attitude.roll = 0.0;
+    this->attitude.pitch = 0.0;
+    this->attitude.yaw = 0.0;
+
+
+    Quaternions quaternions;
+    Vector3d init_euler_angle(this->attitude.roll, this->attitude.pitch, this->attitude.yaw);
+    this->attitude.q_attitude = quaternions.GetQFromEuler(init_euler_angle);
+    Vector3d gyro_coef(0.0,0.0,0.0);
+    this->parameters.gyro_coef = gyro_coef;
+    VectorXd acc_coef(6);
+    acc_coef << 0.0,0.0,0.0,1.0,1.0,1.0;
+    this->parameters.acc_coef = acc_coef;
+    VectorXd mag_coef(6);
+    mag_coef << 0.0,0.0,0.0,1.0,1.0,1.0;
+    this->parameters.mag_coef = mag_coef;
+
+    this->parameters.weak_gps = 100;
+    this->parameters.gamma = 1.0;
+    this->parameters.epsilon = 0.000001;
+    this->parameters.max_step = 200;
+    Vector3d err(0.0,0.0,0.0);
+    this->parameters.err = err;
+    this->parameters.ki = 0.05;
+    this->parameters.kp = 10.0;
+    this->parameters.halfT = 1 / 20.0;
+    this->parameters.g = 9.805567;
+    this->parameters.mag = 157.44;
+    this->parameters.t = 1 / 2.5;
 }
--- system/Status.h
+++ system/Status.h
@@ -2,13 +2,10 @@
 // Created by yangcheng on 2019/1/14.
 //
 
-#include "Eigen/Dense"
 #ifndef LOCATION_STATUS_H
 #define LOCATION_STATUS_H
 
-
-
-using namespace Eigen;
+#include "Eigen/Dense"
 
 struct Position {
     // x,y,z轴的平面位置坐标
@@ -16,7 +13,7 @@ struct Position {
     double y;
     double z;
 
-    // x,y,z轴的球面位置坐标
+    // 经纬度,海拔
     double lng;
     double lat;
     double altitude;
@@ -34,18 +31,24 @@ struct Attitude {
     double roll;
     double pitch;
     double yaw;
+
+    // 姿态四元数
+    Eigen::Vector4d q_attitude;
 };
 
 struct Parameters{
 
+    // 弱GPS精度阈值
+    double weak_gps;
+
     // 陀螺仪标定参数, v(offset_x,offset_y,offset_z)
-    VectorXd gyro_coef;
+    Eigen::VectorXd gyro_coef;
 
     // 加速计标定参数, v(offset_x,offset_y,offset_z,scale_x,scale_y,scale_z)
-    VectorXd acc_coef;
+    Eigen::VectorXd acc_coef;
 
     // 地磁计标定参数, v(offset_x,offset_y,offset_z,scale_x,scale_y,scale_z)
-    VectorXd mag_coef;
+    Eigen::VectorXd mag_coef;
 
     // LM算法标定参数, 对加速计和地磁计通用
     // 初始化阻尼因子用, mu = gamma * max(A), A = Jacobi_t * Jacobi;
@@ -57,16 +60,20 @@ struct Parameters{
 
     // AHRS算法参数
     // 误差积分
-    Vector3d err;
+    Eigen::Vector3d err;
     // PID控制算法参数, 比例参数
     double ki;
     // PID控制算法参数, 积分参数
     double kp;
     // 采样频率的一半
     double halfT;
+    // 采样时间间隔
+    double t;
 
     // 当地重力加速度值
     double g;
+    // 当地场强模值
+    double mag;
 };
 
 class Status {
@@ -84,6 +91,8 @@ public:
     Attitude GetAttitude() const;
 
     Parameters GetParameters() const;
+
+    void Init();
 
 };
 
