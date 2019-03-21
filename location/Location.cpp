6b0c0beac75bc2bcc3178e8ff1f0a6dd7d37589b
yangcheng <yangcheng@zuzuche.com>
2019-03-21 11:05:06 +0800

    添加GPS静态速度判断阈值参数

--- location/Location.cpp
+++ location/Location.cpp
@@ -42,50 +42,50 @@ void Location::PredictCurrentPosition(Vector3d &gyro_data, Vector3d &acc_data, V
     
     // 传感器参数
     Parameters parameters = status.parameters;
-    // 利用标定参数较正当前传感器数据
-    Vector3d gyro_data_cali;
-    Vector3d acc_data_cali;
-    Vector3d mag_data_cali;
-    Vector3d g_data_format;
-    // 较正陀螺仪, 单位rad
-    gyro_data_cali(0) = gyro_data(0) - parameters.gyro_coef(0);
-    gyro_data_cali(1) = gyro_data(1) - parameters.gyro_coef(1);
-    gyro_data_cali(2) = gyro_data(2) - parameters.gyro_coef(2);
-    // 较正加速计, 单位为g
-    acc_data_cali(0) = (acc_data(0) / status.parameters.g - parameters.acc_coef(0)) * parameters.acc_coef(3);
-    acc_data_cali(1) = (acc_data(1) / status.parameters.g - parameters.acc_coef(1)) * parameters.acc_coef(4);
-    acc_data_cali(2) = (acc_data(2) / status.parameters.g - parameters.acc_coef(2)) * parameters.acc_coef(5);
-    // 重力数据归一
-    g_data_format = g_data / status.parameters.g;
-    // 较正地磁计, 单位μt
-    mag_data_cali(0) = (mag_data(0) / 1000.0 - parameters.mag_coef(0)) * parameters.mag_coef(3);
-    mag_data_cali(1) = (mag_data(1) / 1000.0 - parameters.mag_coef(1)) * parameters.mag_coef(4);
-    mag_data_cali(2) = (mag_data(2) / 1000.0 - parameters.mag_coef(2)) * parameters.mag_coef(5);
+//    // 利用标定参数较正当前传感器数据
+//    Vector3d gyro_data_cali;
+//    Vector3d acc_data_cali;
+//    Vector3d mag_data_cali;
+//    Vector3d g_data_format;
+//    // 较正陀螺仪, 单位rad
+//    gyro_data_cali(0) = gyro_data(0) - parameters.gyro_coef(0);
+//    gyro_data_cali(1) = gyro_data(1) - parameters.gyro_coef(1);
+//    gyro_data_cali(2) = gyro_data(2) - parameters.gyro_coef(2);
+//    // 较正加速计, 单位为g
+//    acc_data_cali(0) = (acc_data(0) / status.parameters.g - parameters.acc_coef(0)) * parameters.acc_coef(3);
+//    acc_data_cali(1) = (acc_data(1) / status.parameters.g - parameters.acc_coef(1)) * parameters.acc_coef(4);
+//    acc_data_cali(2) = (acc_data(2) / status.parameters.g - parameters.acc_coef(2)) * parameters.acc_coef(5);
+//    // 重力数据归一
+//    g_data_format = g_data / status.parameters.g;
+//    // 较正地磁计, 单位μt
+//    mag_data_cali(0) = (mag_data(0) / 1000.0 - parameters.mag_coef(0)) * parameters.mag_coef(3);
+//    mag_data_cali(1) = (mag_data(1) / 1000.0 - parameters.mag_coef(1)) * parameters.mag_coef(4);
+//    mag_data_cali(2) = (mag_data(2) / 1000.0 - parameters.mag_coef(2)) * parameters.mag_coef(5);
+////
+////    Vector3d g(0, 0, 1.0);
 //
-//    Vector3d g(0, 0, 1.0);
-
-    // 获取姿态
-    AHRS ahrs;
-    Vector4d q_attitude;
-    if(this->status.parameters.ins_count == 0) {
-        Quaternions quaternions;
-        q_attitude = quaternions.GetQFromEuler(ornt_data);
-    } else {
-        q_attitude = status.attitude.q_attitude;
-    }
-    Vector3d *err = &status.parameters.err;
-    double ki = status.parameters.ki;
-    double kp = status.parameters.kp;
-    double halfT = status.parameters.halfT;
-//    StrapdownAHRS strapdownAHRS;
-//    Vector4d attitude = strapdownAHRS.StrapdownUpdateAttitude(q_attitude, gyro_data, &status);
-    Vector4d attitude = ahrs.UpdateAttitude(err, q_attitude, gyro_data_cali, g_data, mag_data_cali, ki, kp, halfT);
-    // 更新姿态
-    status.attitude.q_attitude = attitude;
+//    // 获取姿态
+//    AHRS ahrs;
+//    Vector4d q_attitude;
+//    if(this->status.parameters.ins_count == 0) {
+//        Quaternions quaternions;
+//        q_attitude = quaternions.GetQFromEuler(ornt_data);
+//    } else {
+//        q_attitude = status.attitude.q_attitude;
+//    }
+//    Vector3d *err = &status.parameters.err;
+//    double ki = status.parameters.ki;
+//    double kp = status.parameters.kp;
+//    double halfT = status.parameters.halfT;
+////    StrapdownAHRS strapdownAHRS;
+////    Vector4d attitude = strapdownAHRS.StrapdownUpdateAttitude(q_attitude, gyro_data, &status);
+//    Vector4d attitude = ahrs.UpdateAttitude(err, q_attitude, gyro_data_cali, g_data, mag_data_cali, ki, kp, halfT);
+//    // 更新姿态
+//    status.attitude.q_attitude = attitude;
 
     // 加速计从b系转到n系
-    Quaternions quaternions;
-    attitude = quaternions.GetQFromEuler(ornt_data);
+//    Quaternions quaternions;
+//    attitude = quaternions.GetQFromEuler(ornt_data);
 //    Matrix3d newRotated_b2n = quaternions.GetDCMFromQ(attitude);
 //    Vector3d acc_b = acc_data_cali - g_data_format;
 //    Vector3d final_acc = newRotated_b2n * acc_b * status.parameters.g;
@@ -102,8 +102,8 @@ void Location::PredictCurrentPosition(Vector3d &gyro_data, Vector3d &acc_data, V
     // 更新惯性位置,速度
     Accelerometer accelerometer;
 //    accelerometer.PositionIntegral(&status, final_acc_lpf, status.parameters.t);
-//    Quaternions quaternions;
-//    attitude = quaternions.GetQFromEuler(ornt_data);
+    Quaternions quaternions;
+    Vector4d attitude = quaternions.GetQFromEuler(ornt_data);
     accelerometer.StrapdownUpdateVelocityPosition(&status, acc_data, attitude, g_data);
 
     // 获取GPS精度
@@ -137,9 +137,13 @@ void Location::PredictCurrentPosition(Vector3d &gyro_data, Vector3d &acc_data, V
         status.position.altitude = gps_data(2);
         status.attitude.yaw = gps_bearing;
         gps.UpdateVelocity(&status, gps_speed, gps_bearing);
-        // 更新相关参数值
+        // 更新相关参数值,gps(lng,lat,alt,accuracy,speed,bearing,t)
         status.parameters.gps_pre_lng = gps_data(0);
         status.parameters.gps_pre_lat = gps_data(1);
+        status.parameters.gps_pre_altitude = gps_data(2);
+        status.parameters.gps_pre_accuracy = gps_data(3);
+        status.parameters.gps_pre_speed = gps_speed;
+        status.parameters.gps_pre_bearing = gps_bearing;
         status.parameters.gps_pre_t = gps_data(6);
         status.parameters.gps_count += 1;
         status.parameters.ins_count = 0;
@@ -162,7 +166,7 @@ void Location::PredictCurrentPosition(Vector3d &gyro_data, Vector3d &acc_data, V
 void Location::SetHz(double f) {
     this->status.parameters.acc_hz = f / 2.0;
     this->status.parameters.halfT = 1.0 / (f * 2.0);
-    this->status.parameters.t = 1.0 / (f * 1.8);
+    this->status.parameters.t = 1.0 / (f * 0.3);
 }
 
 
