de7b916b30b902db378e3bd9b4ceb68ffe1f23a4
yangcheng <yangcheng@zuzuche.com>
2019-04-15 17:11:14 +0800

    :art: update, 删除冗余代码

--- sensor/Gravity.cpp
+++ sensor/Gravity.cpp
@@ -14,7 +14,7 @@ bool Gravity::IsShaking(routing::Status *status, Eigen::Vector3d &g_data) {
     static int cnt = 0;
 
 
-    if(cnt <= 20.0){
+    if(cnt <= (*status).parameters.Hz){
         pre_g_data.push(g_data);
         cnt += 1;
 //        std::cout << g_data(0) << " " << g_data(1) << " " << g_data(2) << " " << 0 << " " << 0 << std::endl;
@@ -24,19 +24,7 @@ bool Gravity::IsShaking(routing::Status *status, Eigen::Vector3d &g_data) {
         pre_g_data.pop();
         pre_g_data.push(g_data);
 //        std::cout << g_data(0) << " " << g_data(1) << " " << g_data(2) << " " << diff_pre_curent << " " << (diff_pre_curent > 0.5) << std::endl;
-        return diff_pre_curent > 0.5;
+        return diff_pre_curent > (*status).parameters.shaking_threshold;
     }
-
-//    cnt += 1;
-//    std::cout.precision(9);
-//    if(cnt % 20 == 0.0){
-//        double diff_pre_curent = (g_data - pre_g_data).squaredNorm();
-//        pre_g_data = g_data;
-//        std::cout << g_data(0) << " " << g_data(1) << " " << g_data(2) << " " << diff_pre_curent << " " << (diff_pre_curent > 0.1) << std::endl;
-//        return diff_pre_curent > 0.1;
-//    }else{
-//        std::cout << g_data(0) << " " << g_data(1) << " " << g_data(2) << " " << 0 << " " << 0 << std::endl;
-//        return false;
-//    }
 
 }
