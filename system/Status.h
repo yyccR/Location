82b1f2e31f811ca68f781bfccbf633caefc4032b
yangcheng <yangcheng@zuzuche.com>
2019-04-22 16:01:35 +0800

    add the crossroad distance param for orientation compensation.

--- system/Status.h
+++ system/Status.h
@@ -149,6 +149,10 @@ namespace routing {
         int max_ignore_in_tunnel;
         // 距路口多少米时开始侧重指南针,在此之前gps可用则用,其次是道路方向
         double min_dist_to_cross;
+        // 距下个路口距离
+        double dist_to_next_cross;
+        // 距离上个路口距离
+        double dist_from_pre_cross;
 
         // 当地重力加速度值
         double g;
